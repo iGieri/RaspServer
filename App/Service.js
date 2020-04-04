ff16a53724f7d5b422e40d49886f0c66e3932a6b
Federico Gerardi <federico.gerardi03@gmail.com>
2020-04-05 00:39:13 +0200

    It works, Icons and more,,,

--- App/Service.js
+++ App/Service.js
@@ -3,11 +3,14 @@ import { Text, View, StyleSheet, ToastAndroid } from "react-native";
 import ServiceButton from "./ServiceButton";
 import Nav from "./Nav";
 import io from "socket.io-client";
+import Button from "./Button";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { useNavigation } from '@react-navigation/native';
 
 const styles = StyleSheet.create({
     container: {
         justifyContent: "center",
-        alignContent: "center"
+        alignItems: "center"
     },
     service: {
         position: "relative",
@@ -37,22 +40,41 @@ const styles = StyleSheet.create({
         color: "#fff",
         fontFamily: "PressStart2P-Regular",
         fontSize: 10,
-        width: 300,
+        width: 320,
         paddingVertical: 20,
-        paddingLeft: 20,
+        paddingHorizontal: 20,
         borderRadius: 10,
         position: "relative",
-        left: 25,
+    
         top: 60
-    }
+    },
+    turnOnButton: {
+        position: "relative",
+        top: 80,
+        width: 57,
+        textAlign: "center"
+    },
+    refreshButton: {
+        position: "relative",
+        top: 100,
+        width: 57,
+        textAlign: "center"
+    },
+    arrowButton: {
+        position: "relative",
+        top: 120,
+        width: 57,
+        textAlign: "center"
+    },
 });
 
 
-export default class Service extends Component {
+class Service extends Component {
 
     constructor(props){
         super(props);
         this.state = {status: ""};
+        
     }
 
     componentDidMount() {
@@ -68,6 +90,7 @@ export default class Service extends Component {
     }
 
     render() {
+        const { navigation } = this.props;
         if (this.state.status == "ON") {
             return(
                 <View style={styles.container}>
@@ -76,20 +99,34 @@ export default class Service extends Component {
                     <Text style={styles.service}>Service: <Text style={styles.on}>ON</Text></Text>
                     <Text style={styles.logTitle}>Console:</Text>
                     <Text style={styles.console}>{this.state.console}</Text>
+                    <Button style={styles.turnOnButton}><Icon name="power-off" color="#ffffff" size={20} /></Button>
+                    <Button style={styles.refreshButton}><Icon name="refresh" color="#ffffff" size={20} /></Button>
+                    <Button style={styles.arrowButton}><Icon name="arrow-left" color="#ffffff" size={20} /></Button>
                 </View>
             );
         }
 
         else {
             return(
-                <View style={styles.container}>
+                <View>
                     <Nav />
                     <ServiceButton title={this.props.title} image={this.props.image} noBorder={true}>{this.props.description}</ServiceButton>
-                    <Text style={styles.service}>Service: <Text style={styles.off}>OFF</Text></Text>
-                    <Text style={styles.logTitle}>Console:</Text>
-                    <Text style={styles.console}>{this.state.console}</Text>
+                    <View style={styles.container}>
+                        <Text style={styles.service}>Service: <Text style={styles.off}>OFF</Text></Text>
+                        <Text style={styles.logTitle}>Console:</Text>
+                        <Text style={styles.console}>{this.state.console}</Text>
+                        <Button style={styles.turnOnButton}><Icon name="power-off" color="#ffffff" size={20} /></Button>
+                        <Button style={styles.refreshButton}><Icon name="refresh" color="#ffffff" size={20} /></Button>
+                        <Button onPress={() => {navigation.navigate("Home");}} style={styles.arrowButton}><Icon name="arrow-left" color="#ffffff" size={20} /></Button>
+                    </View>
                 </View>
             );
         }
     }
+}
+
+export default function(props) {
+    const navigation = useNavigation();
+
+    return <Service navigation={navigation} {...props} />
 }
