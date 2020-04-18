87e4a3b331861ef0b7295160aa8617d073214449
Federico Gerardi <federico.gerardi03@gmail.com>
2020-04-18 18:19:57 +0200

    website

---
+++ docs/precache-manifest.732c26683f26102b94621d61152a9953.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d65e7f8cf776c719d5978ea05e5782",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97879aa05cef0f8155f4",
+    "url": "/static/css/main.5c484bd8.chunk.css"
+  },
+  {
+    "revision": "3d1cfcc29daa0c7dbd22",
+    "url": "/static/js/2.1e079ef0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e079ef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97879aa05cef0f8155f4",
+    "url": "/static/js/main.7c5e4ae1.chunk.js"
+  },
+  {
+    "revision": "b30e1f670b058069f14f",
+    "url": "/static/js/runtime-main.485de5b4.js"
+  },
+  {
+    "revision": "3cf3ff7e606595d079924f216d059400",
+    "url": "/static/media/backgroud_header.3cf3ff7e.jpg"
+  },
+  {
+    "revision": "b38e6d878df7ecbaf56548be1caf7f01",
+    "url": "/static/media/logo.b38e6d87.png"
+  }
+]);
